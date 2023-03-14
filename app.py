70c9194661bce8e7d524607f73ec38720c7a2814
Anagha Yodtare <yodtaana@uw.edu>
2023-03-13 17:29:56 -0700

    Created a web application that will generate an activity based on the input provided by the user about desired number of participants and price range.

---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, request
+import requests
+import random
+
+app = Flask(__name__)
+
+
+activities = [
+    "Take a walk outside",
+    "Read a book",
+    "Listen to music",
+    "Meditate",
+    "Do some yoga",
+    "Bake cookies",
+    "Draw a picture",
+    "Watch a movie",
+    "Call a friend",
+]
+
+def bored(input_activity):
+    matching_activities = [activity for activity in activities if input_activity.lower() in activity.lower()]
+    if matching_activities:
+        return random.choice(matching_activities)
+    else:
+        return random.choice(activities)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    participants = request.form['participants']
+    price_range = request.form.get('price')
+
+    params = {}
+    if price_range:
+        min_price, max_price = price_range.split(',')
+        params['minprice'] = min_price
+        params['maxprice'] = max_price
+
+    response = requests.get("https://www.boredapi.com/api/activity",
+                            params={"participants": participants, "price": price_range})
+    data = response.json()
+
+    if 'activity' in data:
+        activity = data['activity']
+        activity_type = data['type']
+        return render_template('results.html', activity=activity)
+    else:
+        suggestions = []
+        for i in range(3):
+            response = requests.get("https://www.boredapi.com/api/activity")
+            data = response.json()
+            suggestions.append(data['activity'])
+        return render_template('results.html', suggestions=suggestions)
+
+@app.route('/results')
+def results():
+    activity = request.args.get('activity')
+    suggestion_count = int(request.args.get('suggestion_count'))
+
+
+    activity = bored(activity)
+
+
+    if not activity:
+        suggestions = random.choices(activities, k=suggestion_count)
+    else:
+        suggestions = random.choices(activities, k=suggestion_count - 1)
+
+    return render_template('results.html', activity=activity, suggestions=suggestions)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
